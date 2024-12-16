a3ca960afaaea9487550b5f77d559da4a7ea8f06
md-asik-reza-tasin <mdasikreza1997@gmail.com>
2024-12-17 00:33:28 +0600

    Add Backend ---- Pagination, Implement : Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Product from "./Product";
 
 function App() {
   const [pro, setPro] = useState([]);
   const [item, setItem] = useState([]);
+  const [searchProduct, setSearchProduct] = useState("");
+
+  const search = useRef();
 
   //DATA LOAD --------------------------------
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
+    fetch(`http://localhost:5000/pagination?search=${searchProduct}`)
       .then((res) => res.json())
-      .then((data) => setPro(data.products));
-  }, []);
+      .then((data) => setPro(data));
+  }, [searchProduct]);
 
-  const page = [...Array(pro.length / 6).keys()];
-  // console.log(page);
-  // console.log(pro);
-  // console.log(item);
+  const page = [...Array(5).keys()];
+  // (page);
+  // (pro);
+  // (item);
 
   //PAGINATION
 
   const handlePagination = (pageNum) => {
     const lastProductIndex = pageNum * 6;
     const firstProductIndex = lastProductIndex - 6;
-    const currentProduct = pro.slice(firstProductIndex, lastProductIndex);
-    setItem(currentProduct);
+    // const currentProduct = pro.slice(firstProductIndex, lastProductIndex);
+    fetch(
+      `http://localhost:5000/paginations?last=${lastProductIndex}&first=${firstProductIndex}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setItem(data);
+        setPro(data);
+      });
   };
+
+  // (searchProduct, pro);
 
   return (
     <div>
-      {item.length ? (
+      <input
+        onChange={() => setSearchProduct(search.current.value)}
+        ref={search}
+        className="w-48 h-10 p-5 border"
+        type="search"
+        placeholder="Search"
+      />
+      {searchProduct.length === 0 ? (
         <div className="grid grid-cols-3 gap-3">
           {item?.map((product, idx) => (
             <Product key={idx} product={product}></Product>
@@ -37,22 +56,24 @@ function App() {
         </div>
       ) : (
         <div className="grid grid-cols-3 gap-3">
-          {pro?.slice(0, 6)?.map((product, idx) => (
+          {pro?.map((product, idx) => (
             <Product key={idx} product={product}></Product>
           ))}
         </div>
       )}
-      <div className="my-12">
-        {page.map((num, idx) => (
-          <button
-            onClick={() => handlePagination(idx + 1)}
-            className="btn mr-2"
-            key={idx}
-          >
-            {num + 1}
-          </button>
-        ))}
-      </div>
+      {searchProduct.length === 0 && (
+        <div className="my-12">
+          {page?.map((num, idx) => (
+            <button
+              onClick={() => handlePagination(idx + 1)}
+              className="btn mr-2"
+              key={idx}
+            >
+              {num + 1}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,18 +1,15 @@
 export default function Product({ product }) {
-//   console.log(product);
+  //   console.log(product);
 
-  const {thumbnail} = product;
+  const { thumbnail, title } = product;
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
-        <img
-          src={thumbnail}
-          alt="Shoes"
-        />
+        <img src={thumbnail} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title"></h2>
-        <p></p>
+        <p>{title}</p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">BUY NOW</button>
         </div>
